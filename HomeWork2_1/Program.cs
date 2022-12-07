d9120eea759826ec75a39491376ab54560227cc2
Aleksandr <nozhnikov89@mail.ru>
2022-12-07 21:07:58 +0300

    add homework1_2

---
+++ HomeWork2_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает
+// на вход трехзначное число и на выходе
+// показывает вторую цифру этого числа.
+
+int num = new Random().Next(100,1000);
+Console.WriteLine(num);
+
+int result = (num / 10) % 10;
+
+Console.WriteLine(result);
